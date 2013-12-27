e95d692be9430cb40862f211bf21686e432bd568
Andy Pickler <andy.pickler@gmail.com>
2013-12-27 07:48:01 -0700

    Start at 14.3, p202

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Depot::Application.routes.draw do
-  get "admin/index"
-  get "sessions/new"
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   get "sessions/create"
   get "sessions/destroy"
   resources :users
-
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   get "store/index"
@@ -16,11 +18,10 @@ Depot::Application.routes.draw do
     get :who_bought, on: :member
   end
 
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
   root 'store#index', as: 'store'
 
   # Example of regular route:
